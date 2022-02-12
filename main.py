4cab263d037f749651427545db22d843aba37dff
Nathan Patton <npatton@gmail.com>
2022-02-12 10:02:57 -0600

    smaller dataset of more common words

--- main.py
+++ main.py
@@ -1,78 +1,163 @@
 from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
 
+from common_five_letter_words import common_words as common_five_letter_words
 from five_letter_words import words as five_letter_words
 
 
 @dataclass
 class ValidCharacter:
-    character: str
     definite_locations: set[int]
     definite_not_locations: set[int]
 
 
-ELIMINATED_CHARACTERS = {
-    "a",
-    "i",
-    "u",
-}
-
-INCLUDED_CHARACTERS = [
-    ValidCharacter(
-        "d",
-        {2},
-        {0, 1, 3, 4}
-    ),
-    ValidCharacter(
-        "e",
-        {0, 3},
-        {2}
-    ),
-    ValidCharacter(
-        "r",
-        {4},
-        {2, 3}
-    )
-]
+class CharacterStatus(Enum):
+    GRAY = "gray"
+    GREEN = "green"
+    YELLOW = "yellow"
 
 
-def print_possible_words():
-    possible_words = []
+@dataclass
+class CharacterGuess:
+    character: str
+    status: CharacterStatus
 
-    for word in five_letter_words:
-        if len(set(word).intersection(ELIMINATED_CHARACTERS)) > 0:
-            continue
 
-        is_valid: bool = True
+class WordGame:
+    _eliminated_characters: set[str]
+    _included_characters: dict[str, ValidCharacter]
+    possible_words: set[str]
+    possible_common_words: set[str]
 
-        for valid_character in INCLUDED_CHARACTERS:
+    def __init__(
+        self,
+        possible_words: Optional[set[str]],
+        possible_common_words: Optional[set[str]],
+    ):
+        self._eliminated_characters = set()
+        self._included_characters = {}
+        self.possible_words = possible_words
+        self.possible_common_words = possible_common_words
+
+    def make_guess(self, guess: list[CharacterGuess]):
+        for index, character_guess in enumerate(guess):
+            self._update_characters(index, character_guess)
+
+        self._update_possible_words()
+
+    def print_possible_answers(self):
+        if len(self.possible_words) == 1:
+            print(f"The answer is {self.possible_words.pop().upper()}.")
+            return
+
+        possible_answers: list[str] = list(self.possible_words)
+        possible_answers.sort()
+        print(f"There are {len(possible_answers)} possible answers.")
+        print("\n".join(possible_answers))
+        print()
+
+        if len(self.possible_common_words) == 1:
+            print(f"The answer is probably {self.possible_common_words.pop().upper()}.")
+            return
+
+        possible_common_answers: list[str] = list(self.possible_common_words)
+        possible_common_answers.sort()
+        print(f"There are {len(possible_common_answers)} common possible answers.")
+        print("\n".join(possible_common_answers))
+
+    def _update_characters(self, position: int, guess: CharacterGuess):
+        if guess.status == CharacterStatus.GRAY:
+            self._eliminated_characters.add(guess.character)
+            return
+
+        value = self._included_characters.get(
+            guess.character, ValidCharacter(set(), set())
+        )
+
+        if guess.status == CharacterStatus.YELLOW:
+            value.definite_not_locations.add(position)
+        else:
+            value.definite_locations.add(position)
+
+        self._included_characters[guess.character] = value
+
+    def _update_possible_words(self):
+        updated_possible_words: set[str] = set()
+        updated_possible_common_words: set[str] = set()
+
+        for word in self.possible_words:
+            if len(set(word).intersection(self._eliminated_characters)) > 0:
+                continue
+
+            is_valid: bool = True
+
+            for character, valid_character in self._included_characters.items():
+                if not is_valid:
+                    break
+
+                if character not in word:
+                    is_valid = False
+                    break
+
+                for invalid_location in valid_character.definite_not_locations:
+                    if word[invalid_location] == character:
+                        is_valid = False
+                        break
+
+                for valid_location in valid_character.definite_locations:
+                    if word[valid_location] != character:
+                        is_valid = False
+                        break
+
             if not is_valid:
-                break
+                continue
 
-            if valid_character.character not in word:
-                is_valid = False
-                break
+            updated_possible_words.add(word)
 
-            for invalid_location in valid_character.definite_not_locations:
-                if word[invalid_location] == valid_character.character:
-                    is_valid = False
-                    break
+            if word in self.possible_common_words:
+                updated_possible_common_words.add(word)
 
-            for valid_location in valid_character.definite_locations:
-                if word[valid_location] != valid_character.character:
-                    is_valid = False
-                    break
-
-        if not is_valid:
-            continue
-
-        possible_words.append(word)
-
-    possible_words.sort()
-    print("\n".join(possible_words))
+        self.possible_words = updated_possible_words
+        self.possible_common_words = updated_possible_common_words
 
 
 def main():
-    print_possible_words()
+    word_game: WordGame = WordGame(five_letter_words, common_five_letter_words)
+
+    # Guess #1 - ADIEU
+    word_game.make_guess(
+        [
+            CharacterGuess("a", CharacterStatus.YELLOW),
+            CharacterGuess("d", CharacterStatus.GRAY),
+            CharacterGuess("i", CharacterStatus.GRAY),
+            CharacterGuess("e", CharacterStatus.GRAY),
+            CharacterGuess("u", CharacterStatus.YELLOW),
+        ]
+    )
+
+    # Guess #2 - QUACK
+    word_game.make_guess(
+        [
+            CharacterGuess("q", CharacterStatus.GRAY),
+            CharacterGuess("u", CharacterStatus.YELLOW),
+            CharacterGuess("a", CharacterStatus.YELLOW),
+            CharacterGuess("c", CharacterStatus.GRAY),
+            CharacterGuess("k", CharacterStatus.GRAY),
+        ]
+    )
+
+    # Guess #3 - FAULT
+    word_game.make_guess(
+        [
+            CharacterGuess("f", CharacterStatus.GRAY),
+            CharacterGuess("a", CharacterStatus.YELLOW),
+            CharacterGuess("u", CharacterStatus.YELLOW),
+            CharacterGuess("l", CharacterStatus.YELLOW),
+            CharacterGuess("t", CharacterStatus.YELLOW),
+        ]
+    )
+    word_game.print_possible_answers()
 
 
 if __name__ == "__main__":
